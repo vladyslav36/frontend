1854f84bee4aa19d6da3a685ac0acf3387433f04
Vladyslav <vladyslav36@gmail.com>
2022-03-31 20:30:01 +0300

    3003

--- components/Navbar.js
+++ components/Navbar.js
@@ -40,43 +40,45 @@ export default function Navbar({ categories: categoriesProps }) {
     <div className={styles.container}>
       {categories.length ? (
         <ul className={styles.list}>
-          {categories.map(
-            (category) =>
-              category.parentCategoryId === null && (
-                <li
-                  className={styles.category}
-                  key={category._id}
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    router.push(`/category/${category.slug}`)
-                  }}
-                  onMouseEnter={(e) => getSpace(e)}
-                >
-                  {category.name}
-                  {getChildren(category, categories).length ? (
-                    <ul
-                      className={
-                        styles.drop_down_list +
-                        " " +
-                        (isShowRight ? styles.right_side : styles.left_side)
-                      }
-                    >
-                      {getChildren(category, categories).map((item) => (
-                        <li
-                          onClick={(e) => {
-                            e.stopPropagation()
-                            router.push(`/category/${item.slug}`)
-                          }}
-                          key={item._id}
-                        >
-                          {item.name}&nbsp;({item.qntProducts})
-                        </li>
-                      ))}
-                    </ul>
-                  ) : null}
-                </li>
-              )
-          )}
+          {categories
+            .sort((a, b) => (a.name > b.name ? 1 : -1))
+            .map(
+              (category) =>
+                category.parentCategoryId === null && (
+                  <li
+                    className={styles.category}
+                    key={category._id}
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      router.push(`/category/${category.slug}`)
+                    }}
+                    onMouseEnter={(e) => getSpace(e)}
+                  >
+                    {category.name}
+                    {getChildren(category, categories).length ? (
+                      <ul
+                        className={
+                          styles.drop_down_list +
+                          " " +
+                          (isShowRight ? styles.right_side : styles.left_side)
+                        }
+                      >
+                        {getChildren(category, categories).map((item) => (
+                          <li
+                            onClick={(e) => {
+                              e.stopPropagation()
+                              router.push(`/category/${item.slug}`)
+                            }}
+                            key={item._id}
+                          >
+                            {item.name}&nbsp;({item.qntProducts})
+                          </li>
+                        ))}
+                      </ul>
+                    ) : null}
+                  </li>
+                )
+            )}
         </ul>
       ) : null}
     </div>
