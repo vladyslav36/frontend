747a0bbe48433b734f72ffdf95c2a1e86307a941
Vladyslav <vladyslav36@gmail.com>
2022-04-03 08:09:01 +0300

    03041

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL 
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"
 export const NOIMAGE = '/noimage.png'
 export const PHONE1='+38 050 950 16 71'
 export const PHONE2='+38 098 208 60 83'
