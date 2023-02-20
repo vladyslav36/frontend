c8d06a05da45fc119cbd9d908e640525b3438136
vladyslav36 <vladyslav36@ukr.net>
2023-02-20 09:26:32 +0200

    20022023

--- components/LoginBot.js
+++ components/LoginBot.js
@@ -20,8 +20,7 @@ export default function LoginBot({ elemDialog }) {
     setAuthKey(uuid().replace(/-/gi, ""))
   },[user])
 
-  socket.on("authkey", (arg) => {   
-    
+  socket.on("authkey", (arg) => {       
     setArg(arg)   
   })
 
