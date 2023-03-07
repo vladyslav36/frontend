3fca47d41ddbd454523caeb62df998b47a75cd39
vladyslav36 <vladyslav36@ukr.net>
2023-03-07 06:36:33 +0200

    07032023

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='ru'>
       <Head>
+        
         <link rel="shortcut icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital@0;1&display=swap"
