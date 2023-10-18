65e007124b81379e0bda96217173778b217e6604
vladyslav36 <vladyslav36@ukr.net>
2023-10-18 11:56:09 +0300

    18102023

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import styles from "@/styles/Navbar.module.scss"
-import { useContext, useEffect, useLayoutEffect, useRef, useState } from "react"
+import { useContext, useLayoutEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { API_URL } from "../config"
 import ProductsContext from "@/context/ProductsContext"
@@ -7,62 +7,24 @@ import ProductsContext from "@/context/ProductsContext"
 export default function Navbar() {
   const { catToggle, setCatToggle } = useContext(ProductsContext)
   const router = useRouter()
-  const [isShowRight, setIsShowRight] = useState(false)
-  const [categories, setCategories] = useState([])
-  const [catalogs, setCatalogs] = useState([])
+  const [categoriesList, setCategoriesList] = useState([])
+  const [catalogsList, setCatalogsList] = useState([])
 
   const fakeArray = Array(20)
   fakeArray.fill("")
 
   useLayoutEffect(() => {
     const fetcher = async () => {
-      const res = await fetch(`${API_URL}/api/categories`)
+      const res = await fetch(`${API_URL}/api/categories/navbar`)
       if (res.ok) {
-        const { categories } = await res.json()
-        setCategories(categories)
+        const { categoriesList, catalogsList } = await res.json()
+        setCategoriesList(categoriesList)
+        setCatalogsList(catalogsList)
       }
     }
 
     fetcher()
   }, [])
-
-  useLayoutEffect(() => {
-    const fetcher = async () => {
-      const res = await fetch(`${API_URL}/api/catalogs`)
-      if (res.ok) {
-        const { catalogs } = await res.json()
-        setCatalogs(catalogs)
-      }
-    }
-
-    fetcher()
-  }, [])
-
-  const getChildren = (cat, catArray) => {
-    return catArray.filter((item) => item.parentId === cat._id)
-  }
-
-  // Вычисление с какой стороны показывать выпадающий список справа или слева
-  // const getSpace = (e) => {
-  //   const rightSpace = window.innerWidth - e.target.getBoundingClientRect().x
-  //   setIsShowRight(rightSpace < 160 ? true : false)
-  // }
-  // categoriesList список категорий формата {cat:категория-бренд,children:ее первые дети}
-  const getCatList = (catArray) =>
-    catArray
-      .filter((item) => item.parentId === null)
-      .sort((a, b) => (a.name > b.name ? 1 : -1))
-      .map((cat) => {
-        return {
-          cat: cat,
-          children: getChildren(cat, catArray).sort((a, b) =>
-            a.name > b.name ? 1 : -1
-          ),
-        }
-      })
-
-  const categoriesList = getCatList(categories)
-  const catalogsList = getCatList(catalogs)
 
   return (
     <>
@@ -102,33 +64,11 @@ export default function Navbar() {
                     className={styles.category}
                     onClick={(e) => {
                       e.stopPropagation()
-                      router.push(`/category/${item.cat._id}`)
+                      router.push(`/category/${item._id}`)
                     }}
-                    // onMouseEnter={(e) => getSpace(e)}
                   >
-                    <p>{item.cat.name}</p>
+                    <p>{item.name}</p>
                   </div>
-                  {/* {item.children.length ? (
-                    <div
-                      className={
-                        styles.drop_down_list +
-                        " " +
-                        (isShowRight ? styles.right_side : styles.left_side)
-                      }
-                    >
-                      {item.children.map((child, i) => (
-                        <div
-                          onClick={(e) => {
-                            e.stopPropagation()
-                            router.push(`/category/${child._id}`)
-                          }}
-                          key={i}
-                        >
-                          {child.name}&nbsp;({child.qntProducts})
-                        </div>
-                      ))}
-                    </div>
-                  ) : null} */}
                 </div>
               ))}
               {fakeArray.map((item, i) => (
@@ -144,33 +84,11 @@ export default function Navbar() {
                   className={styles.category}
                   onClick={(e) => {
                     e.stopPropagation()
-                    router.push(`/catalog/${item.cat._id}`)
+                    router.push(`/catalog/${item._id}`)
                   }}
-                  // onMouseEnter={(e) => getSpace(e)}
                 >
-                  <p>{item.cat.name}</p>
+                  <p>{item.name}</p>
                 </div>
-                {/* {item.children.length ? (
-                  <div
-                    className={
-                      styles.drop_down_list +
-                      " " +
-                      (isShowRight ? styles.right_side : styles.left_side)
-                    }
-                  >
-                    {item.children.map((child, i) => (
-                      <div
-                        onClick={(e) => {
-                          e.stopPropagation()
-                          router.push(`/catalog/${child._id}`)
-                        }}
-                        key={i}
-                      >
-                        {child.name}&nbsp;({child.qntProducts})
-                      </div>
-                    ))}
-                  </div>
-                ) : null} */}
               </div>
             ))}
             {fakeArray.map((item, i) => (
